9b35632aea1196a4543ac0d43eaae00e60709620
Mark Talbot <markjtalbot@gmail.com>
2017-06-05 22:46:37 -0400

    Basic implementation of vue component template

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        My template is loaded: {{ isLoaded }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            isLoaded: true
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid black;
+    }
+</style>
